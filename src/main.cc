a5c52d2178c4cec4bf2ae82fbeca7b438edea3ed
Jan Hensel <ja_he@uni-bremen.de>
2021-03-20 12:08:14 +0100

    Structure reading in image into class
    Screw performance concerns, i want neatness!
    Long term it might be worth looking into how we could structure the
    memory the image readin gives us to be neatly accessible (or maybe
    just wrap the nasty (line_idx*width*3)+(pixel_idx*3)+color_idx
    calculation in there? We could obviously free the memory in the
    destructor, and I'm not even sure we need to keep it around the
    whole time, since Ideally we'll abstract these values before
    clustering or whatever.
    
    I've rudimentarily tested the correctness of how we're using the
    stb-function by comparing the value of the bottom right pixel to the
    value GIMP tells me for it in a reference image:
      Pixel bottom_right = img.at(img.get_height()-1,img.get_width()-1);
      printf("bottom right pixel has values (%d,%d,%d)\n",
            bottom_right.r,
            bottom_right.g,
            bottom_right.b
          );
    This turned up (3,255,138) just as GIMP told me, so we know we don't
    have our RGBs twisted around or have any skew where it gives us 4
    values instead of the 3 we ask for (because this ref img was a png
    with 4 channels).
    Seems to be fine.

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,7 @@
+#include <filesystem>
 #include <iostream>
+
+#include <vector>
 
 #include <algorithm>
 #include <numeric>
@@ -9,16 +12,68 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+struct Pixel
+{
+  uint8_t r = 0;
+  uint8_t g = 0;
+  uint8_t b = 0;
+};
+typedef std::vector<Pixel> Line;
+
+class Image
+{
+public:
+  Image(std::filesystem::path img_path)
+  {
+    uint8_t* rgb_image = stbi_load(
+      img_path.c_str(), &this->width, &this->height, &this->n_channels, 3);
+    if (n_channels != 3) {
+      std::cerr << "WARNING (reading in image): number of channels in image is "
+                   "not 3 but "
+                << n_channels << std::endl;
+    }
+
+    this->img.resize(height);
+    std::for_each(
+      img.begin(), img.end(), [this](auto& line) { line.resize(width); });
+
+    for (int li = 0; li < height; li++) {
+      for (int pi = 0; pi < width; pi++) {
+        this->img[li][pi] = { rgb_image[(li * width * 3) + (pi * 3) + (0)],
+                              rgb_image[(li * width * 3) + (pi * 3) + (1)],
+                              rgb_image[(li * width * 3) + (pi * 3) + (2)] };
+      }
+    }
+
+    stbi_image_free(rgb_image);
+  };
+
+  const Pixel& at(size_t line, size_t pixel) const
+  {
+    return this->img[line][pixel];
+  };
+
+  int get_width(void) const { return this->width; };
+  int get_height(void) const { return this->height; };
+  int get_n_channels(void) const { return this->n_channels; };
+
+private:
+  int               width;
+  int               height;
+  int               n_channels;
+  std::vector<Line> img;
+};
+
 int
 main(int argc, char* argv[])
 {
   assert(argc > 1);
 
-  int width, height, n_channels;
-  uint8_t* rgb_image = stbi_load(argv[1], &width, &height, &n_channels, 3);
-  assert(n_channels == 3);
-
-  stbi_image_free(rgb_image);
+  Image img(argv[1]);
+  printf("read in a %d by %d image with %d channels.\n",
+         img.get_width(),
+         img.get_height(),
+         img.get_n_channels());
 
   return 0;
 }
