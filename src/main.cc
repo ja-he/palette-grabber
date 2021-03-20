a8b67a10c85a6700e127763324efa400672ad75d
Jan Hensel <ja_he@uni-bremen.de>
2021-03-20 11:35:46 +0100

    Write a simple image printout
    Prints the read in image back out as ascii-art comprised of [ .-+#].
    Obviously only makes sense for small-ish images (maybe max 200x200).
    
    This is pointless for the purpose of the project but I like it.

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,36 @@
+#include <iostream>
+
+#include <algorithm>
+#include <numeric>
+
+#include <stdint.h>
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-int
-main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+    int width, height, n_channels;
+
+    uint8_t* rgb_image = stbi_load(argv[1], &width, &height, &n_channels, 3);
+    std::cout << "read in a " << width << "x" << height << " image with " << n_channels << " channels." << std::endl;
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        uint32_t value = 0;
+        for (int c = 0; c < 3; c++) {
+          value += rgb_image[(i*width*3)+(j*3)+c];
+        }
+        value = value / 3;
+        char c;
+        if      (value <  50) c = ' ';
+        else if (value < 100) c = '.';
+        else if (value < 150) c = '-';
+        else if (value < 200) c = '+';
+        else                  c = '#';
+        std::cout << c << c;
+      }
+      std::cout << std::endl;
+    }
+
+    stbi_image_free(rgb_image);
+
+    return 0;
 }
