a1ecd2d01a5b22d067263e03e145caf144e23ade
Jan Hensel <ja_he@uni-bremen.de>
2021-03-20 23:33:52 +0100

    Define a class OptionSpec and use it
    probably migrate class to header and make part of Options but we'll
    see what makes sense.

--- src/cli.cc
+++ src/cli.cc
@@ -1,6 +1,7 @@
 #include "cli.hh"
 
 #include <map>
+#include <set>
 #include <string>
 
 #include <regex>
@@ -40,22 +41,47 @@ parse_args(int argc, char* argv[])
   return m;
 }
 
-std::map<std::string, std::string> short_to_long{ { "i", "input" },
-                                                  { "d", "dump-colors" },
-                                                  { "n", "naive" } };
+struct OptionSpec
+{
+public:
+  std::map<std::string, std::string> short_to_long{ { "i", "input" },
+                                                    { "d", "dump-colors" },
+                                                    { "n", "naive" } };
+  std::map<std::string, std::string> long_to_short{ { "input", "i" },
+                                                    { "dump-colors", "d" },
+                                                    { "naive", "n" } };
 
-std::map<std::string, std::vector<std::string>> incompatible_counterparts{
-  { "input", {} },
-  { "dump-colors", { "naive" } },
-  { "naive", { "dump-colors" } },
+  std::map<std::string, std::vector<std::string>> incompatible_counterparts{
+    { "input", {} },
+    { "dump-colors", { "naive" } },
+    { "naive", { "dump-colors" } },
+  };
+
+  std::map<std::string, std::string> description{
+    { "input", "The input file. Must be given a valid input file name." },
+    { "dump-colors", "Dump all colors from the image." },
+    { "naive",
+      "Naively get unique (by euclidean distance) color values from image to "
+      "form palette. Can be given a distance threshold value, default is 100.0 "
+      "if none provided." }
+  };
+
+  std::map<std::string, std::string> option_arguments{ { "input", "<file>" },
+                                                       { "dump-colors", "" },
+                                                       { "naive",
+                                                         "[<threshold>]" } };
+
+  std::set<std::string> required{ "input" };
 };
+
+const OptionSpec spec;
 
 Options::Options(std::map<std::string, std::string> m)
 {
   assert(!m.contains(""));
 
   // migrate values to long option names, erase any short ones
-  for (const auto& [s, l] : short_to_long) {
+  for (const auto& [s, l] : spec.short_to_long) {
     if (m.contains(s)) {
       assert(!m.contains(l));
       m[l] = m[s];
@@ -64,8 +90,29 @@ Options::Options(std::map<std::string, std::string> m)
   }
 
   // TODO(ja-he): verify existence of required options
+  for (const auto& required : spec.required) {
+    if (!m.contains(required)) {
+      std::cerr << "Required argument '" << required << "' missing. Exiting."
+                << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
 
   // TODO(ja-he): verify no incompatible options
+  for (const auto& [option, _] : m) {
+    if (!spec.description.contains(option)) {
+      std::cerr << "Unrecognized option '" << option << "'. Exiting."
+                << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    for (const auto& counterpart : spec.incompatible_counterparts.at(option)) {
+      if (m.contains(counterpart)) {
+        std::cerr << "Incompatible arguments '" << option << "' and '"
+                  << counterpart << "'. Exiting." << std::endl;
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
 
   // parse all options
   if (m.contains("naive")) {
